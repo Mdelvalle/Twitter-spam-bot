afe63be893aa4f0ee2f4065fd02d43d7df76d1a3
Miguel Del Valle <mdelvalle@live.com>
2014-04-13 15:45:43 -0500

    First tweets

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -4,6 +4,20 @@ class MicroBlogger
   attr_reader :client
 
   def initialize
-    puts "Initializing"
+    puts "Initializing..."
+    @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+    if message.size > 140
+      puts "Tweet is too big!!"
+    else
+      @client.update(message)
+    end
+  end
+end
+
+if __FILE__ == $0
+  blogger = MicroBlogger.new
+  blogger.tweet("".ljust(140, "z"))
 end
